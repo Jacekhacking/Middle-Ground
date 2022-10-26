1884b7e2ec8075c57b8238a7da4a7679fb3c63ad
JacekHacking <Jacekhacking@mac.com>
2022-10-26 14:39:57 -0600

    reworked homepage layout

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,9 +1,21 @@
 import styles from "../styles/Header.module.css";
+import HeaderTitle from "../images/HeaderTitle.png";
+import Image from "next/image";
+
+const HeaderClass = {
+  // objectFit: "cover",
+};
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>MIDDLE GROUND TREATMENT AND TRAINING</h1>
+      <Image
+        styles={HeaderClass}
+        src={HeaderTitle}
+        alt="Middle Ground Treatment & Training"
+        height={500}
+        width={500}
+      />
     </header>
   );
 };
--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,14 +1,23 @@
 import styles from "../styles/Navbar.module.css";
+import logo from "../images/navbarLogo.png";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <div>Logo</div>
+      <Image src={logo} alt="logo" width="100" height="100" />
       <ul>
         <li>Home</li>
         <li>Treatment</li>
         <li>Training</li>
         <li>Blog</li>
+        <li>About</li>
+        <li>FAQ</li>
+      </ul>
+      <ul>
+        <li>fb</li>
+        <li>fb</li>
+        <li>fb</li>
       </ul>
     </nav>
   );
