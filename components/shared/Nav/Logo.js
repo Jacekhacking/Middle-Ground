ec5c15d894f3a9d567f4d4d65274930c58cc64c8
JacekHacking <Jacekhacking@mac.com>
2022-11-07 12:54:13 -0700

    reformatting navbar adding onhover change for logo

---
+++ components/shared/Nav/Logo.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../..//styles/Navbar.module.css";
+
+import logo from "../../images/navbarLogo.png";
+import invertLogo from "../../images/invertedNavbarLogo.png";
+
+import { useState } from "react";
+
+const Logo = () => {
+  const [isHovering, setIsHovering] = useState(false);
+  const onMouseEnter = () => setIsHovering(true);
+  const onMouseExit = () => setIsHovering(false);
+
+  return (
+    <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseExit}>
+      <Link href="/">
+        {isHovering ? (
+          <Image
+            src={invertLogo}
+            alt="logo"
+            width="100"
+            height="100"
+            className={styles.logo}
+          />
+        ) : (
+          <Image
+            src={logo}
+            alt="logo"
+            width="100"
+            height="100"
+            className={styles.logo}
+          />
+        )}
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
--- components/shared/Nav/MenuItems.js
+++ components/shared/Nav/MenuItems.js
@@ -1,5 +1,46 @@
+import Link from "next/link";
+import { NavData } from "./NavData";
+import styles from "../..//styles/Navbar.module.css";
+
 const MenuItems = ({ items }) => {
-  return <li>{items}</li>;
+  const NavItems = NavData;
+
+  return (
+    <>
+      {NavItems.map((item) =>
+        item.subMenu ? (
+          <div key={item.title} className={styles["navbar-menu-container"]}>
+            <Link href={item.url}>
+              <a>{item.title}</a>
+            </Link>
+            <ul className={styles["nav-ul"]}>
+              {item.subMenu.map((subUrl) => (
+                <Link
+                  href={subUrl.url}
+                  key={subUrl.title}
+                  className={styles["nav-item"]}
+                >
+                  <div className={styles["nav-item"]}>{subUrl.title} </div>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div key={item.title} className={styles["navbar-menu-container"]}>
+            <Link href={item.url}>
+              <a>{item.title}</a>
+            </Link>
+          </div>
+        )
+      )}
+
+      <ul className={styles["nav-socials"]}>
+        <li>fb</li>
+        <li>inst</li>
+        <li>fb</li>
+      </ul>
+    </>
+  );
 };
 
 export default MenuItems;
