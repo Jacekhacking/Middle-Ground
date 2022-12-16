7a88698d2cbac52b15ce87078c33af6e74492623
JacekHacking <Jacekhacking@mac.com>
2022-12-16 13:47:04 -0700

    elements added

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,14 @@ export default function Home() {
         <Header />
 
         <section className={`${styles["welcome-section"]}`}>
-          <h2>HELLO WELCOME TO MY WEBSITE my name is alora</h2>
-
-          {/* <Image alt="photo of myself" src={welcomeImg} width={250} /> */}
-
+          <h2>WE PROVIDE TREATMENT AND TRAINING FOR EVERYONE</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed fugit
-            dolores repellat voluptates laborum reprehenderit iste molestias
-            autem temporibus minus cumque assumenda sequi similique eos dicta
-            laboriosam, dolorem vel recusandae?
+            Middle Ground is part of a health collective that in motivated help
+            yo find your middle ground amidst the various treatment & training
+            paths in the world. We offer bodywork modalities & personal training
+            to help you be your best self, feel good for a lifetime, and build
+            trust within your own body. Book a section to learn the most.
+            Explore the website to learn more.
           </p>
         </section>
 
@@ -28,13 +27,31 @@ export default function Home() {
         </section>
 
         <section className={`${styles["services-section"]}`}>
-          <h3>SERVICES</h3>
+          <h2>SERVICES</h2>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
-            reiciendis aspernatur facilis, dolorem itaque quasi? Vel dolore vero
-            laudantium hic nulla delectus porro quo, voluptatem, labore possimus
-            nisi laboriosam explicabo?
+            Here is a list of the modalities & services offered with Middle
+            Ground we like to take a comprehensive view of the body because we
+            believe that you deserve it all. Each session is customized to each
+            client. Depending on where you are along the spectrum of treatment &
+            training. Book a treatment or a training session.{" "}
           </p>
+          <div className="" style={{ display: "flex" }}>
+            <ul>
+              <h3>TRAINING</h3>
+              <li>Strength Training</li>
+              <li>FRC Training</li>
+              <li>Corrective Exercise</li>
+            </ul>
+            <ul>
+              <h3>TREATMENT</h3>
+              <li>Massage Therapy</li>
+              <li>Assisted Stretching</li>
+              <li>Cupping</li>
+              <li>Muscle Scraping</li>
+              <li>Lymphatic Drainage</li>
+              <li>Deep Tissue</li>
+            </ul>
+          </div>
         </section>
       </main>
     </div>
