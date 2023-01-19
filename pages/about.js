689e5bf86952836d256cba796a40c550824a53a7
JacekHacking <Jacekhacking@mac.com>
2023-01-18 17:40:31 -0700

    basic layout for pages

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,17 @@
 const about = () => {
   return (
-    <>
-      <div>hello from the about Page</div>
-    </>
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+      <h2 className=" text-black text-5xl font-bold underline ">About!</h2>
+      <p className="mt-20 text-black text-center text-2xl max-w-5xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+        provident libero, perferendis facilis, id laudantium, aspernatur dicta
+        quaerat ratione quia voluptates recusandae. Sit molestias eius aut. Quis
+        fuga commodi magni?
+      </p>
+      <button className="mt-20 custom-box-shadow text-xl bg-white text-black  w-32 h-12 hover:bg-black hover:text-white">
+        <a href="">Book Now</a>
+      </button>
+    </div>
   );
 };
 
