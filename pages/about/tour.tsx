1c14c869df4c76569bc433d531130503bfcd2986
JacekHacking <Jacekhacking@mac.com>
2023-04-07 12:34:30 -0600

    carousel sizing and spacing for desktop

--- pages/about/tour.tsx
+++ pages/about/tour.tsx
@@ -2,8 +2,8 @@ import Carousel from "../../components/UI/carousel/Carousel";
 
 const Tour = () => {
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
-      <h2 className="text-black">Check Out Our Space!</h2>
+    <div className="min-h-screen bg-zinc-200 flex flex-col items-center justify-center">
+      <h2 className="text-black ">Check Out Our Space!</h2>
       <Carousel />
     </div>
   );
