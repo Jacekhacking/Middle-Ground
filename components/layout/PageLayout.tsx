6388fb6a454205ef1d09eb0964cfb9d8a09ac2ed
JacekHacking <Jacekhacking@mac.com>
2023-04-18 12:02:18 -0600

    removing PageLayout for deploy

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -1,12 +1,12 @@
-import MainHeader from "./MainHeader";
-import Footer from "./Footer";
-const Layout = ({ children }) => {
-  return (
-    <div className="min-h-screen">
-      <MainHeader />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
-};
-export default Layout;
+// import MainHeader from "./MainHeader";
+// import Footer from "./Footer";
+// const Layout = ({ children }) => {
+//   return (
+//     <div className="min-h-screen">
+//       <MainHeader />
+//       <main>{children}</main>
+//       <Footer />
+//     </div>
+//   );
+// };
+// export default Layout;
